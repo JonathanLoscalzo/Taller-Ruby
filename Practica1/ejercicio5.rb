9a511e26a22e9d7908c937409ae2aa233a9ea78e
JonathanLoscalzo <loscalzo.jony@gmail.com>
2014-09-02 08:26:47 -0300

    ejercicio6

---
+++ Practica1/ejercicio5.rb
@@ -0,0 +1,17 @@
+=begin
+5.  Escriba una función que reciba un hash y retorne un string con las claves y valores del hash formateados en una lista HTML.
+Por ejemplo:
+>  to_html({bananas: 5, naranjas: 10})
+=>  "<ul><li>bananas: 5</li><li>naranjas: 10</li></ul>"
+
+=end
+
+def to_html(hash)
+	html = '<ul>'
+	hash.each { |clave, valor|  html << "<li> #{clave} : #{valor}</li>" }
+	html << '</ul>'
+end
+
+#var = to_html :bananas => 5 , :naranjas => 10
+#var = to_html({:bananas => 5 , :naranjas => 10})
+
---
+++ Practica1/ejercicio6.rb
@@ -0,0 +1,17 @@
+=begin
+6.  Dado un string cualquiera reemplace cada ocurrencia de   
+{  por  do\n 
+ y cada ocurrencia de   
+}  por end 
+. Por ejemplo:
+>  reemplazar("[1, 2, 3, 4].each { |x| a = x**2 + x**3 + x**5\np a\n }")
+=>  "[1, 2, 3, 4].each do\n |x| a = x**2 + x**3 + x**5\np a\n end"
+¿Puede escribir la función reemplazar en una sola linea aprovechando los métodos de instancia de la clase String?
+
+=end
+
+def reemplazar(cadena)
+	return cadena.gsub(/['{}']/, '{'=>"do\n" , '}'=>"end")
+end
+
+#reemplazar("[1, 2, 3, 4].each { |x| a = x**2 + x**3 + x**5\np a\n }")
