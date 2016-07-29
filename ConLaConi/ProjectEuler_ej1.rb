dc5559ff2d98fed09a487ebe0f386a4e2f205d0c
Jonathan.Loscalzo <loscalzo.jony@gmail.com>
2016-07-29 20:20:08 -0300

    borro carpeta

--- ConLaConi/ProjectEuler_ej1.rb
+++
@@ -1,9 +0,0 @@
-
-=begin
-
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-Find the sum of all the multiples of 3 or 5 below 1000.
-=end
-
-(1...1000).reduce(0) { |sum, var| ( var % 3 == 0 || var % 5 == 0) ? sum + var : sum }
